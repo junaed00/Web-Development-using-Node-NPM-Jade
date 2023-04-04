2cda17b195da1def8d73e828c5906ed3439f7729
ss-joy <ssjoy.u.b@gmail.com>
2023-04-04 07:12:22 +0600

    task 4 ok

--- task3/public/scripts/time.js
+++ task3/public/scripts/time.js
@@ -1,10 +1,3 @@
 const p = document.querySelector("p");
 const d = new Date();
 p.innerText = d.toLocaleDateString();
-
-const clock = document.querySelector("#clock");
-
-setInterval(() => {
-  const timee = new Date();
-  clock.innerText = timee.toLocaleTimeString();
-}, 1000);
